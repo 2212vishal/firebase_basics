4450a58001eae64cef86a97c39070ad95f3b4508
VishalEazybe <vishalchaurasiya@eazybe.com>
2024-07-21 17:59:45 +0530

    forth commit

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,13 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./spendwise-ca308-firebase-adminsdk-pbfcj-619b28a129.json'); // Update this path
+const serviceAccount = require(''); // Update this path
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://spendwise-ca308-default-rtdb.firebaseio.com/' // Update this URL to match your Firebase project
+  databaseURL: '' // Update this URL to match your Firebase project
 });
 
 const db = admin.database();
 
 module.exports = db;
+
+
